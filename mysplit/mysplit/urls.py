3f0a8c26b17f4182f5899c0274284e2c0b968695
omar jandali <omarjandali@omars-mbp.lan>
2018-03-07 22:01:03 -0800

    Updated the models and migrations to ensure that application can run without any errors

--- mysplit/mysplit/urls.py
+++ mysplit/mysplit/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls import url, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     # url(r'^general/', include('general.urls')),
-    url(r'^', include('general.urls')),
+    url(r'^', include('users.urls')),
+    url(r'^general/$', include('general.urls')),
 ]
